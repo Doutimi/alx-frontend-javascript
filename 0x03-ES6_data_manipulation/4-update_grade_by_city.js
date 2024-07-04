2c0538e4b1c6092a469b52a3b9c0367363a5cc27
Doutimi <isenahdoutimiebi@gmail.com>
2024-07-04 08:38:44 -0700

    task 10

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.map((student) => {
+    const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+    if (matchingGrade) {
+      return { ...student, grade: matchingGrade.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  }).filter((student) => student.location === city);
+}
